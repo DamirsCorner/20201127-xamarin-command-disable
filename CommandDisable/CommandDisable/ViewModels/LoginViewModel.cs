7d0daaef8ce268f97bd83d5bd216c5c09a7ed0dd
Damir Arh <damir.arh@gmail.com>
2020-11-08 08:18:49 +0100

    Button not disabled initially

--- CommandDisable/CommandDisable/ViewModels/LoginViewModel.cs
+++ CommandDisable/CommandDisable/ViewModels/LoginViewModel.cs
@@ -8,12 +8,37 @@ namespace CommandDisable.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private string username;
+        private string password;
+
         public Command LoginCommand { get; }
 
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
         }
+
+        public string Username
+        { 
+            get => username;
+            set
+            {
+                SetProperty(ref username, value);
+                OnPropertyChanged(nameof(LoginAllowed));
+            }
+        }
+
+        public string Password 
+        { 
+            get => password;
+            set
+            {
+                SetProperty(ref password, value);
+                OnPropertyChanged(nameof(LoginAllowed));
+            }
+        }
+
+        public bool LoginAllowed => !string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password);
 
         private async void OnLoginClicked(object obj)
         {
