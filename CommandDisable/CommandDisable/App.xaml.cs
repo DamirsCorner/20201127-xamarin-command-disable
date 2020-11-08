945b7f6663f8534b37eba05301220de96c8a7e4e
Damir Arh <damir.arh@gmail.com>
2020-11-08 08:15:30 +0100

    Add project files.

---
+++ CommandDisable/CommandDisable/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CommandDisable.Services;
+using CommandDisable.Views;
+
+namespace CommandDisable
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
